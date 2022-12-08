e1b70620cdf8880a8265bf5ac6cd7d895449e6ac
ShamanBeads <creevyconor@gmail.com>
2022-12-08 13:15:43 +0000

    First commit with basic ramdomiser info

---
+++ script.js
@@ -0,0 +1,17 @@
+const randomise = (newName, tribe, role) => {
+
+
+newName = ["Kibonoota", "Amevewaki", "Shimoshani", 
+    "Abadoowaku", "Bilouduma", "Emifazuki", 
+    "Fahrabuzo", "Gibboxuni", "Hamamermo", "Lipuzinatzka"];
+
+tribe = ["The Murky Bones Horde", "The Arcane Claw Caste",
+"The Yellow Claw Warriors", "The Last Axe Clan",
+"The Frozen Owl Tribe", "The Gray Aura Clan",
+"The Red Sea Warriors", "The Angry River Children",
+"The Dead Ember Tribe", "The Blind Swamp Clan"];
+
+role = ["Hunter", "Gatherer", "Shaman", "Chief",
+ "Magician", "Warlord", "Farmer", "Weaponsmith",
+   "Hut Builder", "Water boy" ];
+}
